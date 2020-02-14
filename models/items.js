19667999aa1b4440b25e5fc22fa02831cb853d54
Connor Reese <connor.reese13@gmail.com>
2020-02-14 14:27:47 +0800

    model change

--- models/items.js
+++ models/items.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
 
-// module.exports = mongoose.model("items", {
-//   name: "Bob Ross",
-//   done: true
-// });
-
 let Items = mongoose.model("items", {
   name: String,
   done: Boolean
 });
 
 module.exports = Items;
-
-//module.exports = mongoose.model();
