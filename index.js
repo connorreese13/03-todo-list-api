cc30bd7155a2ba07f2cf3491c092315befec74cf
Connor Reese <connor.reese13@gmail.com>
2020-02-14 15:53:49 +0800

    changes to deploy to heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+
+let port = process.env.PORT;
 
 const app = express();
 app.use(cors());
@@ -11,4 +14,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/items", require("./controllers/items.js"));
 
-app.listen(4000, () => console.log("Ready on port 4000"));
+app.listen(port, () => console.log("Ready on port 4000"));
